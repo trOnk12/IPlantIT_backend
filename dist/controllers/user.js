c801cd75a088fd701aea58863c86b98f61be57e6
Mateusz Pachulski <mateusz.pachulski@appunite.com>
2021-06-12 19:02:32 +0200

    update typescript

---
+++ dist/controllers/user.js
@@ -0,0 +1,51 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.postSignup = void 0;
+const express_validator_1 = require("express-validator");
+const User_1 = require("../models/User");
+/**
+ * Create a new local account.
+ * @route POST /signup
+ */
+const postSignup = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    yield express_validator_1.check("email", "Email is not valid").isEmail().run(req);
+    yield express_validator_1.check("password", "Password must be at least 4 characters long").isLength({ min: 4 }).run(req);
+    yield express_validator_1.check("confirmPassword", "Passwords do not match").equals(req.body.password).run(req);
+    // const errors = validationResult(req);
+    // if(!errors.isEmpty){
+    //     res.status(401).send({
+    //         message: errors.array()
+    //     });
+    // }
+    const user = new User_1.User({
+        email: req.body.email,
+        password: req.body.password
+    });
+    User_1.User.findOne({ email: req.body.email }, (err, existingUser) => {
+        if (err) {
+            return next(err);
+        }
+        if (existingUser) {
+            res.status(401).send({
+                message: 'User already exists.'
+            });
+        }
+        user.save((err) => {
+            if (err) {
+                return next(err);
+            }
+            res.send("User successfully registered");
+        });
+    });
+});
+exports.postSignup = postSignup;
+//# sourceMappingURL=user.js.map
