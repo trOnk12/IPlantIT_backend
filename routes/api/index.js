20faefdee1839b7478f42ac934a2bd59e4a78925
Mateusz Pachulski <mateusz.pachulski@appunite.com>
2021-06-12 16:02:43 +0200

    init npm

--- routes/api/index.js
+++
@@ -1,7 +0,0 @@
-var router = require('express').Router();
-
-router.use('/devices',require('./devices'));
-router.use('/users',require('./user'));
-router.use('/auth',require('./auth'));
-
-module.exports = router;
