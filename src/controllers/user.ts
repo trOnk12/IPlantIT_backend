b1823481c0f7b868a3290d9e5f55d512a9ff59c4
Mateusz Pachulski <mateusz.pachulski@appunite.com>
2021-06-12 18:19:34 +0200

    add simple sign up logic

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,10 +1,37 @@
 import { Request, Response, NextFunction } from "express";
 import { check, sanitize, validationResult } from "express-validator";
+import { NativeError } from "mongoose";
+import { User, UserDocument } from "../models/User";
 
 /**
  * Create a new local account.
  * @route POST /signup
  */
 export const postSignup = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-   
+    await check("email", "Email is not valid").isEmail().run(req);
+    await check("password", "Password must be at least 4 characters long").isLength({ min: 4 }).run(req);
+    await check("confirmPassword", "Passwords do not match").equals(req.body.password).run(req);
+
+    const errors = validationResult(req);
+
+    const user = new User({
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    User.findOne({ email: req.body.email }, (err: NativeError, existingUser: UserDocument) => {
+        if (err) { return next(err); }
+        if (existingUser) {
+            res.status(401).send({
+                message: 'User already exists.'
+            });
+        }
+        user.save((err) => {
+            if (err) { return next(err); }
+
+            res.send("User successfully registered");
+        });
+    });
 };
+
+
